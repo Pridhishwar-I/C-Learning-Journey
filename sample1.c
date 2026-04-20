6878770f85ef60daea348f8102b9df3af529bf4f
Pridhishwar-I <pridhishwari@gmail.com>
2026-04-20 20:18:39 +0530

    Add files via upload

---
+++ sample1.c
@@ -0,0 +1,11 @@
+//Write a C program to print numbers from 1 to 10 using a for loop.
+# include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;++i)
+    {
+        printf("%d\n",i);
+    }
+
+    }
